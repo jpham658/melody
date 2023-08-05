377a1bb70f1a52f28edf0465dcb017090c23e905
Jenny Pham <jpham658@gmail.com>
2023-08-05 20:35:01 +0100

    added queue function

--- src/cogs/music_cog.py
+++ src/cogs/music_cog.py
@@ -8,6 +8,8 @@ load_dotenv()
 
 
 class MusicCog(commands.Cog):
+    EMBED_COLOUR: nextcord.Colour = 0xE5BED0
+
     def __init__(self, bot: commands.Bot) -> None:
         """Constructor for the MusicCog."""
         commands.Cog.__init__(self)
@@ -19,8 +21,10 @@ class MusicCog(commands.Cog):
     async def song(self, ctx: commands.Context):
         if not self.player or not self.player.is_connected():
             return await ctx.send("You're not playing anything.")
-        
-        await ctx.send(f"Currently playing: {self.player.current.title} by {self.player.current.author}")
+
+        await ctx.send(
+            f"Currently playing: {self.player.current.title} by {self.player.current.author}"
+        )
 
     @commands.command()
     async def play(self, ctx: commands.Context, *, track: wavelink.YouTubeTrack = None):
@@ -28,35 +32,30 @@ class MusicCog(commands.Cog):
         If the player is not playing a track, play the track given.
         """
         if not track:
-            return await ctx.send("Please state what song you want to play.") 
-        elif not wavelink.YouTubeTrack.convert(track):
-            return await ctx.send("Track is invalid, please reenter.") 
-        
+            return await ctx.send("Please state what song you want to play.")
+
         if not ctx.voice_client and not ctx.author.voice:
             return await ctx.send("Please enter a voice channel first!")
-        
+
         if ctx.voice_client:
             vc: nextcord.VoiceChannel = ctx.voice_client
-        else: 
+        else:
             vc: nextcord.VoiceChannel = ctx.author.voice.channel
 
         if not self.player or not self.player.is_connected():
-            self.player: wavelink.Player = await vc.connect(timeout=90, reconnect=False, cls=wavelink.Player)
+            self.player: wavelink.Player = await vc.connect(
+                timeout=90, reconnect=False, cls=wavelink.Player
+            )
 
-        if self.player.is_playing():
-            await ctx.send("I'm already playing something!")
-        elif self.player.current:
-            await ctx.send("You need to unpause your current track.")
-        else:
+        # Embed for message
+        embed: nextcord.Embed = await self.__create_embed(track)
+
+        if self.player.queue.is_empty and not self.player.is_playing():
             await self.player.play(track)
-            embed: nextcord.Embed = nextcord.Embed(colour=0xe5bed0, 
-                                                    title=f"{track.title} by {track.author}", 
-                                                    description=f"{track.duration}",
-                                                    url=track.uri)
-            thumbnail: str = await track.fetch_thumbnail()
-            if thumbnail: 
-                embed.set_thumbnail(thumbnail)
-            await ctx.send(content="`˚₊‧꒰ა ♡ ໒꒱ ‧₊˚`", embed=embed)
+            await ctx.send(content="Currently playing:", embed=embed)
+        else:
+            await self.player.queue.put_wait(track)
+            await ctx.send(content="Queued:", embed=embed)
 
     @commands.command()
     async def pause(self, ctx: commands.Context):
@@ -65,28 +64,26 @@ class MusicCog(commands.Cog):
         """
         if not ctx.voice_client and not ctx.author.voice:
             return await ctx.send("Please enter a voice channel first!")
-        
+
         if ctx.voice_client:
             vc: nextcord.VoiceChannel = ctx.voice_client
-        else: 
+        else:
             vc: nextcord.VoiceChannel = ctx.author.voice.channel
 
-        print("passed vc check")
-
         if not self.player or not self.player.is_connected():
-            self.player: wavelink.Player = await vc.connect(timeout=90, reconnect=False, cls=wavelink.Player)
-        
-        print("passed player connected check")
-        print(f"is paused: {self.player.is_paused()}")
+            self.player: wavelink.Player = await vc.connect(
+                timeout=90, reconnect=False, cls=wavelink.Player
+            )
+
         if not self.player.current:
-            return await ctx.send("You're not playing anything!") 
+            return await ctx.send("You're not playing anything!")
         elif self.player.is_paused():
-            return await ctx.send("Your track is already paused!") 
-        
-        print("before pause")
+            return await ctx.send("Your track is already paused!")
+
         await self.player.pause()
-        await ctx.send(f"Paused {self.player.current.title} by {self.player.current.author}")
-        print("after pause")
+        await ctx.send(
+            f"Paused {self.player.current.title} by {self.player.current.author}"
+        )
 
     @commands.command()
     async def resume(self, ctx: commands.Context):
@@ -95,24 +92,26 @@ class MusicCog(commands.Cog):
         """
         if not ctx.voice_client and not ctx.author.voice:
             return await ctx.send("Please enter a voice channel first!")
-        
+
         if ctx.voice_client:
             vc: nextcord.VoiceChannel = ctx.voice_client
-        else: 
+        else:
             vc: nextcord.VoiceChannel = ctx.author.voice.channel
 
-        print("passed vc check")
-
         if not self.player or not self.player.is_connected():
-            self.player: wavelink.Player = await vc.connect(timeout=90, reconnect=False, cls=wavelink.Player)
+            self.player: wavelink.Player = await vc.connect(
+                timeout=90, reconnect=False, cls=wavelink.Player
+            )
 
         if not self.player.current:
             return await ctx.send("You're not playing anything!")
         elif not self.player.is_paused():
-            return await ctx.send("You're already playing something!") 
-        
+            return await ctx.send("You're already playing something!")
+
         await self.player.resume()
-        await ctx.send(f"Resumed playing {self.player.current.title} by {self.player.current.author}")
+        await ctx.send(
+            f"Resumed playing {self.player.current.title} by {self.player.current.author}"
+        )
 
     @commands.command()
     async def disconnect(self, ctx: commands.Context):
@@ -124,7 +123,7 @@ class MusicCog(commands.Cog):
             return await ctx.send("You haven't played anything yet.")
         elif not self.player.is_connected():
             return await ctx.send("I've already disconnected from the channel.")
-        
+
         print("passed player isn't none check")
         await ctx.voice_client.disconnect()
         await ctx.send("Successfully disconnected from VC.")
@@ -136,9 +135,9 @@ class MusicCog(commands.Cog):
 
         if self.player.queue.is_empty:
             return await ctx.send("Queue is empty.")
-        
+
         await self.player.queue.shuffle()
-    
+
     @commands.command()
     async def clear(self, ctx: commands.Context) -> None:
         if not self.player or not self.player.is_connected():
@@ -146,10 +145,10 @@ class MusicCog(commands.Cog):
 
         if self.player.queue.is_empty:
             return await ctx.send("Queue is already empty.")
-        
+
         await self.player.queue.reset()
         await ctx.send("Queue is now empty.")
-    
+
     @commands.command()
     async def skip(self, ctx: commands.Context):
         if not self.player or not self.player.is_connected():
@@ -157,16 +156,14 @@ class MusicCog(commands.Cog):
         if self.player.queue.is_empty:
             return await ctx.send("Queue is empty.")
 
-    @commands.command()
-    async def queue(self, ctx: commands.Context, *, track: wavelink.YouTubeTrack=None):
-        if not self.player or not self.player.is_connected():
-            return await ctx.send("You're not playing anything!")
-        if not track:
-            return await ctx.send("Please state the song you want to queue!")
-        elif not wavelink.YouTubeTrack.convert(track):
-            return await ctx.send("Track is invalid, please reenter.") 
-        
-        self.player.queue
+        # Play the next track if available
+        if not self.player.queue.is_empty:
+            next_track: wavelink.YouTubeTrack = self.player.queue.get()
+            await self.player.play(next_track)
+            await ctx.send("Skipped song!")
+        else:
+            await ctx.send("Queue is empty!")
+
     @commands.Cog.listener()
     async def on_disconnect(self):
         if self.player and self.player.is_connected():
@@ -178,19 +175,21 @@ class MusicCog(commands.Cog):
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, node: wavelink.Node) -> None:
         print(f"Node {node.id} is ready.")
-    
+
     @commands.Cog.listener()
     async def on_wavelink_track_start(self, payload: wavelink.TrackEventPayload):
         print(f"Player has started playing a track.")
-    
+
     @commands.Cog.listener()
     async def on_wavelink_track_end(self, payload: wavelink.TrackEventPayload):
         print(f"Player has finished playing a track.")
-    
+
     @commands.Cog.listener()
-    async def on_wavelink_websocket_closed(self, payload: wavelink.WebsocketClosedPayload):
+    async def on_wavelink_websocket_closed(
+        self, payload: wavelink.WebsocketClosedPayload
+    ):
         print(f"Websocket has closed successfully.")
-    
+
     async def node_connect(self):
         """Connect node to the Lavalink server."""
         await self.bot.wait_until_ready()
@@ -200,6 +199,18 @@ class MusicCog(commands.Cog):
             uri=NODE_HOST, password=NODE_PASSWORD, use_http=True
         )
         await wavelink.NodePool.connect(client=self.bot, nodes=[node])
+
+    async def __create_embed(self, track: wavelink.YouTubeTrack) -> nextcord.Embed:
+        embed: nextcord.Embed = nextcord.Embed(
+            colour=self.EMBED_COLOUR,
+            title=f"{track.title} by {track.author}",
+            description=f"{track.duration}",
+            url=track.uri,
+        )
+        thumbnail: str = await track.fetch_thumbnail()
+        if thumbnail:
+            embed.set_thumbnail(thumbnail)
+        return embed
 
 
 def setup(bot):
